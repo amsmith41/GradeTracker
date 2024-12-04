c0462843233751b96fae5529870ab612c27e3bd4
Derek Williams <derek.williams@wsu.edu>
2024-12-04 12:53:03 -0800

    fixed instlal dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,5 @@
 
 #!/bin/bash
-sudo apt install postgresql postgresql-contrib
-apt install python3.10-venv
-sudo apt install libpq-dev python3-dev
-pip install psycopg2
+sudo apt install postgresql postgresql-contrib -y
+apt install python3.10-venv -y
+sudo apt install libpq-dev python3-dev -y
---
+++ install_python_packages.sh
@@ -0,0 +1,4 @@
+#~/bin/bash
+python3 -m pip install django
+python3 -m pip install python-decouple
+python3 -m pip install psycopg2-binary
